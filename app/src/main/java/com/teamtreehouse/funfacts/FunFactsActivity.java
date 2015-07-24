0577ee15d1a9d250d056576c5db589113b29f232
Ben Deitch <bdeitch@teamtreehouse.com>
2015-07-24 11:28:19 -0400

    Remove Menu code

--- app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -50,26 +50,4 @@ public class FunFactsActivity extends Activity {
         };
         showFactButton.setOnClickListener(listener);
     }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_fun_facts, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
