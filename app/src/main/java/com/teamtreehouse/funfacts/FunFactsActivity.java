3b12a63ac2b6dcdcb7e4d9879c2b85633f6aaefd
Ben Deitch <bdeitch@teamtreehouse.com>
2015-07-24 11:21:21 -0400

    Use an array for our fun facts

--- app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
+++ app/src/main/java/com/teamtreehouse/funfacts/FunFactsActivity.java
@@ -26,20 +26,24 @@ public class FunFactsActivity extends Activity {
         View.OnClickListener listener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String[] facts = {
+                        "Ants stretch when they wake up in the morning.",
+                        "Ostriches can run faster than horses.",
+                        "Olympic gold medals are actually made mostly of silver.",
+                        "You are born with 300 bones; by the time you are an adult you will have 206.",
+                        "It takes about 8 minutes for light from the Sun to reach Earth.",
+                        "Some bamboo plants can grow almost a meter in just one day.",
+                        "The state of Florida is bigger than England.",
+                        "Some penguins can leap 2-3 meters out of the water.",
+                        "On average, it takes 66 days to form a new habit.",
+                        "Mammoths still walked the earth when the Great Pyramid was being built." };
+
                 String fact = "";
                 // Update the label with our dynamic fact
                 Random randomGenerator = new Random();
-                int randomNumber = randomGenerator.nextInt(3);
+                int randomNumber = randomGenerator.nextInt(facts.length);
 
-                if (randomNumber == 0) {
-                    fact = "Ants stretch when they wake up in the morning.";
-                } else if (randomNumber == 1) {
-                    fact = "Ostriches can run faster than horses";
-                } else if (randomNumber == 2) {
-                    fact = "Olympic gold medals are actually made mostly of silver";
-                } else {
-                    fact = "Sorry, there was an error.";
-                }
+                fact = facts[randomNumber];
 
                 factLabel.setText(fact);
             }
