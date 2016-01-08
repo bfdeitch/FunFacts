fa88b79a50bdcf708301520188ecc5a17a740d26
e146152 <Shawn.Kraft.sk@gmail.com>
2016-01-08 12:44:47 -0700

    Added Bronze Background

--- app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
+++ app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
@@ -19,6 +19,7 @@ public class ColorWheel {
             "#e0ab18", // mustard
             "#637a91", // dark gray
             "#f092b0", // pink
+            "#8b4513", // bronze
             "#b7c0c7"  // light gray
     };
 
