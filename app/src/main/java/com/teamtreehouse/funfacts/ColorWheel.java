f5d678726dccb3d6870ee3bc12216ceec7807c79
chickendude <pokemaster103@gmail.com>
2016-10-08 12:17:52 +0800

    Add gold color

--- app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
+++ app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
@@ -19,7 +19,8 @@ public class ColorWheel {
             "#e0ab18", // mustard
             "#637a91", // dark gray
             "#f092b0", // pink
-            "#b7c0c7"  // light gray
+            "#b7c0c7",  // light gray
+            "#ddca8b"   // gold
     };
 
     // Method (abilities: things the object can do)
