8dfb2afaa7c17094af775f5be5768e6cc7a2d73a
Joaquin Cardenas <jcardenas@octetostudios.com>
2017-04-07 19:37:50 -0300

    Add color bronze

--- app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
+++ app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
@@ -20,6 +20,7 @@ public class ColorWheel {
             "#637a91", // dark gray
             "#f092b0", // pink
             "#b7c0c7"  // light gray
+            "#8b4513"   // bronze
     };
 
     // Method (abilities: things the object can do)
