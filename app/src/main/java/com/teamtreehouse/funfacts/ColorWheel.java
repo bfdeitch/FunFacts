29ec3636a2cf27f609fcde23af387be905457733
Stephen Cornett <stephen.t.cornett@outlook.com>
2017-04-28 15:37:45 -0400

    Added gold

--- app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
+++ app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
@@ -19,12 +19,14 @@ public class ColorWheel {
             "#e0ab18", // mustard
             "#637a91", // dark gray
             "#f092b0", // pink
-            "#b7c0c7"  // light gray
+            "#b7c0c7",  // light gray
+            "#ddca8b"   //gold
+
     };
 
     // Method (abilities: things the object can do)
     public int getColor() {
-        String color = "";
+        String color = "1";
 
         // Randomly select a fact
         Random randomGenerator = new Random(); // Construct a new Random number generator
