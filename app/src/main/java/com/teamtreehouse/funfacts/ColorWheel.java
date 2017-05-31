b5c878429166deef9591abcf66b9e5602074720c
sournary <sournaryvd@gmail.com>
2017-05-31 10:37:53 +0700

    add frist color

--- app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
+++ app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
@@ -19,7 +19,8 @@ public class ColorWheel {
             "#e0ab18", // mustard
             "#637a91", // dark gray
             "#f092b0", // pink
-            "#b7c0c7"  // light gray
+            "#b7c0c7", // light gray
+            "#000000"  // first color
     };
 
     // Method (abilities: things the object can do)
