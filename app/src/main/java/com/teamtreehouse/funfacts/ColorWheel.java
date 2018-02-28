dea0de81b42320c85940c5d1dfc5cc0daa6a387a
Victor Cuc <victorcuc99@yahoo.com>
2018-02-28 12:32:22 +0000

    bronze color2

--- app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
+++ app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
@@ -20,7 +20,7 @@ public class ColorWheel {
             "#637a91", // dark gray
             "#f092b0", // pink
             "#b7c0c7", // light gray
-            "#ddca8b" //gold
+            "#8b4613" //bronze2
     };
 
     // Method (abilities: things the object can do)
