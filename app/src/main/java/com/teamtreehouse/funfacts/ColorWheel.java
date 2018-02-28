6ca6c090cc2c4a3e4854d1653f46ef92501df3c0
Victor Cuc <victorcuc99@yahoo.com>
2018-02-28 13:07:42 +0000

    cok

--- app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
+++ app/src/main/java/com/teamtreehouse/funfacts/ColorWheel.java
@@ -21,6 +21,7 @@ public class ColorWheel {
             "#f092b0", // pink
             "#b7c0c7", // light gray
             "#8b4624" //bronze5
+            //cok
     };
 
     // Method (abilities: things the object can do)
